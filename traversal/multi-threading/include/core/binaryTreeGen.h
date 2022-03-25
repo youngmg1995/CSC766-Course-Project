dcaeabd2bcc8241b6255ad86f14f5e4af8b3cf5f
Mitchell Young <youngmg3000@gmail.com>
2022-03-24 21:35:40 -0400

    starting adding multi-threading

---
+++ traversal/multi-threading/include/core/binaryTreeGen.h
@@ -0,0 +1,60 @@
+/******************************************************************************* 
+------------------------------------- INFO -------------------------------------
+*******************************************************************************/
+/**
+ * @file binaryTreeGen.h
+ * @author Mitchell Young (mgyoung@ncsu.edu)
+ * @brief Includes the types and functionalites necessary to generate binary
+ * 	trees. The generated trees can be random or balanced, and can also be
+ * 	generic binary trees or binary search trees.
+ * @version 0.1
+ * @date 2022-03-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef	__BINARYTREE_GEN_H
+#define	__BINARYTREE_GEN_H
+
+
+
+/******************************************************************************* 
+------------------------------- IMPORTS & PARAMS -------------------------------
+*******************************************************************************/
+#include "types.h"
+
+
+
+/******************************************************************************* 
+--------------------------------- DECLARATIONS ---------------------------------
+*******************************************************************************/
+/* basic tree generators */ 
+extern Tree * genRandomTree(int N, bool isBST);
+extern Tree * genContRandomTree(Tree *btNodeArray, int N, bool isBST);
+extern Tree * genBalancedTree(int depth, bool isBST);
+extern Tree * genContBalancedTree(Tree *btNodeArray, int depth, bool isBST);
+
+/* optimized tree generators */ 
+extern TreeInfo genRandomTreeOptimized(int *invTable, ITNode *itNodeArray, int N, bool isBST);
+extern TreeInfo genContRandomTreeOptimized(int *invTable, Tree *btNodeArray, ITNode *itNodeArray, int N, bool isBST);
+extern TreeInfo genBalancedTreeOptimized(int *invTable, ITNode *itNodeArray, int depth, bool isBST);
+extern TreeInfo genContBalancedTreeOptimized(int *invTable, Tree *btNodeArray, ITNode *itNodeArray, int depth, bool isBST);
+
+/* other useful functions */ 
+extern Tree * invTab2BT(int *invTable, int N);
+extern Tree * invTab2ContBT(int *invTable, Tree *btNodeArray, int N);
+extern Tree * invTab2BTOptimized(int *invTable, ITNode *itNodeArray, int N);
+extern Tree * invTab2ContBTOptimized(int *invTable, Tree *btNodeArray, ITNode *itNodeArray, int N);
+extern void convert2BST(Tree *root);
+extern void genTree2StdOut(int N);
+extern void genInversionTable(int *invTable, int N);
+extern void printInvTab(int *invTable, int N, bool vert);
+extern void initITNode(ITNode *node);
+
+
+
+#endif
+/******************************************************************************* 
+--------------------------------- END OF FILE ----------------------------------
+*******************************************************************************/
---
+++ traversal/multi-threading/include/exp/timer.h
@@ -0,0 +1,70 @@
+/******************************************************************************* 
+------------------------------------- INFO -------------------------------------
+*******************************************************************************/
+/**
+ * @file types.h
+ * @author Mitchell Young (mgyoung@ncsu.edu)
+ * @brief Defines binary tree types and other types used to create/draw them.
+ * @version 0.1
+ * @date 2022-03-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef	__BINARYTREE_TIMER_H
+#define	__BINARYTREE_TIMER_H
+
+
+/******************************************************************************* 
+------------------------------- IMPORTS & PARAMS -------------------------------
+*******************************************************************************/
+#include "exp.h"
+
+
+/******************************************************************************* 
+---------------------------------- FUNC DECL -----------------------------------
+*******************************************************************************/
+
+/* functions for timing each tree traversal */
+extern TimeInfo timeTraversal(
+	TreeInfo treeInfo, TraversalFunc traversalFunc, int samples, 
+	bool printResults, bool verbose, const char treeType[], const char storageType[],
+	const char traversalName[], const char callbackName[]
+);
+
+extern TimeInfo timeTraversalCB(
+	TreeInfo treeInfo, TraversalFuncCB traversalFunc, TreeCallback callback, int samples, 
+	bool printResults, bool verbose, const char treeType[], const char storageType[],
+	const char traversalName[], const char callbackName[]
+);
+
+extern TimeInfo timeTraversalLevel(
+	TreeInfo treeInfo, TreeQueue *treeQueue, TraversalFuncLevel traversalFunc, int samples,
+	bool printResults, bool verbose, const char treeType[], const char storageType[],
+	const char traversalName[], const char callbackName[]
+);
+
+extern TimeInfo timeTraversalLevelCB(
+	TreeInfo treeInfo, TreeQueue *treeQueue, TraversalFuncLevelCB traversalFunc, TreeCallback callback, int samples, 
+	bool printResults, bool verbose, const char treeType[], const char storageType[],
+	const char traversalName[], const char callbackName[]
+);
+
+extern TimeInfo timeTraversalCont(
+	TreeInfo treeInfo, Tree *btNodeArray, TraversalFuncCont traversalFunc, int samples, 
+	bool printResults, bool verbose, const char treeType[], const char storageType[],
+	const char traversalName[], const char callbackName[]
+);
+
+extern TimeInfo timeTraversalContCB(
+	TreeInfo treeInfo,  Tree *btNodeArray, TraversalFuncContCB traversalFunc, TreeCallback callback, int samples, 
+	bool printResults, bool verbose, const char treeType[], const char storageType[],
+	const char traversalName[], const char callbackName[]
+);
+
+
+#endif
+/******************************************************************************* 
+--------------------------------- END OF FILE ----------------------------------
+*******************************************************************************/
